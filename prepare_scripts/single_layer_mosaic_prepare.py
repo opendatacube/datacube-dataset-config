d943e5801f817444e9a7bdf3515f2590da9798fa
Alfredo-AMA <alfredo.h.delos_santos@ama-inc.com>
2017-03-28 11:02:43 -0400

    renamed some files, modified ingestion config to match Pilar's, minor modifications to script.

--- prepare_scripts/single_layer_mosaic_prepare.py
+++ prepare_scripts/single_layer_mosaic_prepare.py
@@ -18,11 +18,11 @@ from osgeo import osr
 import os
 import gdal
 import sys
-from osgeo import gdal,ogr,osr
+from osgeo import gdal, ogr, osr
 from datetime import datetime
 
 
-def GetExtent(gt,cols,rows):
+def GetExtent(gt, cols, rows):
     ''' Return list of corner coordinates from a geotransform
 
         @type gt:   C{tuple/list}
@@ -34,20 +34,20 @@ def GetExtent(gt,cols,rows):
         @rtype:    C{[float,...,float]}
         @return:   coordinates of each corner
     '''
-    ext=[]
-    xarr=[0,cols]
-    yarr=[0,rows]
+    ext = []
+    xarr = [0, cols]
+    yarr = [0, rows]
 
     for px in xarr:
         for py in yarr:
-            x=gt[0]+(px*gt[1])+(py*gt[2])
-            y=gt[3]+(px*gt[4])+(py*gt[5])
-            ext.append([x,y])
+            x = gt[0] + (px * gt[1]) + (py * gt[2])
+            y = gt[3] + (px * gt[4]) + (py * gt[5])
+            ext.append([x, y])
         yarr.reverse()
     return ext
 
 
-def ReprojectCoords(coords,src_srs,tgt_srs):
+def ReprojectCoords(coords, src_srs, tgt_srs):
     ''' Reproject a list of x,y coordinates.
 
         @type geom:     C{tuple/list}
@@ -59,26 +59,26 @@ def ReprojectCoords(coords,src_srs,tgt_srs):
         @rtype:         C{tuple/list}
         @return:        List of transformed [[x,y],...[x,y]] coordinates
     '''
-    trans_coords=[]
-    transform = osr.CoordinateTransformation( src_srs, tgt_srs)
-    for x,y in coords:
-        x,y,z = transform.TransformPoint(x,y)
-        trans_coords.append([x,y])
+    trans_coords = []
+    transform = osr.CoordinateTransformation(src_srs, tgt_srs)
+    for x, y in coords:
+        x, y, z = transform.TransformPoint(x, y)
+        trans_coords.append([x, y])
     return trans_coords
 
 
 def getExtents(nbar_path, prep_file):
     ds = gdal.Open(str(nbar_path) + "/" + prep_file)
-    gt=ds.GetGeoTransform()
+    gt = ds.GetGeoTransform()
     cols = ds.RasterXSize
     rows = ds.RasterYSize
-    ext=GetExtent(gt,cols,rows)
+    ext = GetExtent(gt, cols, rows)
 
-    src_srs=osr.SpatialReference()
+    src_srs = osr.SpatialReference()
     src_srs.ImportFromWkt(ds.GetProjection())
     tgt_srs = src_srs.CloneGeogCS()
 
-    geo_ext=ReprojectCoords(ext,src_srs,tgt_srs)
+    geo_ext = ReprojectCoords(ext, src_srs, tgt_srs)
     return geo_ext
 
 
@@ -97,7 +97,7 @@ def prep_dataset(nbar_path, prep_file, processing_level, product_type, platform_
     coord_upper_right = extents[3]
     coord_lower_left = extents[1]
     coord_lower_right = extents[2]
-    
+
     with rasterio.open(str(nbar_path) + "/" + prep_file) as src:
         left = src.bounds.left
         right = src.bounds.right
@@ -107,83 +107,129 @@ def prep_dataset(nbar_path, prep_file, processing_level, product_type, platform_
     band_dict = processBandParams(bands)
     src_ds = gdal.Open(str(nbar_path) + "/" + prep_file)
     images = {}
-    for band in range( src_ds.RasterCount ):
+    for band in range(src_ds.RasterCount):
         band += 1
         srcband = src_ds.GetRasterBand(band)
         images[str(band_dict[str(band)])] = {'path': prep_file, 'layer': srcband.GetBand()}
-        
+
     doc = {
         'id': str(uuid.uuid4()),
         'processing_level': str(processing_level),
-        'product_type': str(product_type), 
-        'creation_dt':  datetime.strptime(prep_file.split("_")[3]+'-01-01 00:00:00', '%Y-%m-%d %H:%M:%S'),
-        'platform': {'code': str(platform_code)},
-        'instrument': {'name': str(instrument)},
+        'product_type': str(product_type),
+        'creation_dt': datetime.strptime(prep_file.split("_")[3] + '-01-01 00:00:00', '%Y-%m-%d %H:%M:%S'),
+        'platform': {
+            'code': str(platform_code)
+        },
+        'instrument': {
+            'name': str(instrument)
+        },
         'extent': {
-            'from_dt': datetime.strptime(prep_file.split("_")[3]+'-01-01 00:00:00', '%Y-%m-%d %H:%M:%S'),
-            'to_dt': datetime.strptime(prep_file.split("_")[3]+'-01-01 00:00:00', '%Y-%m-%d %H:%M:%S'),
-            'center_dt': datetime.strptime(prep_file.split("_")[3]+'-01-01 00:00:00', '%Y-%m-%d %H:%M:%S'),
+            'from_dt': datetime.strptime(prep_file.split("_")[3] + '-01-01 00:00:00', '%Y-%m-%d %H:%M:%S'),
+            'to_dt': datetime.strptime(prep_file.split("_")[3] + '-01-01 00:00:00', '%Y-%m-%d %H:%M:%S'),
+            'center_dt': datetime.strptime(prep_file.split("_")[3] + '-01-01 00:00:00', '%Y-%m-%d %H:%M:%S'),
             'coord': {
-                'll': {'lat': coord_lower_left[1], 'lon': coord_lower_left[0]},
-                'lr': {'lat': coord_lower_right[1], 'lon': coord_lower_right[0]},
-                'ul': {'lat': coord_upper_left[1], 'lon': coord_upper_left[0]},
-                'ur': {'lat': coord_upper_right[1], 'lon': coord_upper_right[0]},
+                'll': {
+                    'lat': coord_lower_left[1],
+                    'lon': coord_lower_left[0]
+                },
+                'lr': {
+                    'lat': coord_lower_right[1],
+                    'lon': coord_lower_right[0]
+                },
+                'ul': {
+                    'lat': coord_upper_left[1],
+                    'lon': coord_upper_left[0]
+                },
+                'ur': {
+                    'lat': coord_upper_right[1],
+                    'lon': coord_upper_right[0]
+                },
             }
         },
-        'format': {'name': 'GeoTiff'},
+        'format': {
+            'name': 'GeoTiff'
+        },
         'grid_spatial': {
             'projection': {
-                'spatial_reference': str(src.crs_wkt),
+                'spatial_reference': str(src.crs),
                 'geo_ref_points': {
-                    'ul': {'x': left, 'y': top},
-                    'ur': {'x': right, 'y': top},
-                    'll': {'x': left, 'y': bottom},
-                    'lr': {'x': right, 'y': bottom},
+                    'ul': {
+                        'x': left,
+                        'y': top
+                    },
+                    'ur': {
+                        'x': right,
+                        'y': top
+                    },
+                    'll': {
+                        'x': left,
+                        'y': bottom
+                    },
+                    'lr': {
+                        'x': right,
+                        'y': bottom
+                    },
                 }
             }
         },
         'image': {
-            'satellite_ref_point_start': {'x': prep_file.split("_")[1], 'y': prep_file.split("_")[2]},
-            'satellite_ref_point_end': {'x': prep_file.split("_")[1], 'y': prep_file.split("_")[2]},
+            'satellite_ref_point_start': {
+                'x': prep_file.split("_")[1],
+                'y': prep_file.split("_")[2]
+            },
+            'satellite_ref_point_end': {
+                'x': prep_file.split("_")[1],
+                'y': prep_file.split("_")[2]
+            },
             'bands': images
         },
-        'lineage': {'source_datasets': {}}
+        'lineage': {
+            'source_datasets': {}
+        }
     }
 
     if instrument is None:
-        doc.pop("instrument", None) # Pop the key if there is no entry
+        doc.pop("instrument", None)  # Pop the key if there is no entry
     return doc
-            
 
-@click.command(help="Prepare Layered GeoTIFF ingestion into the Data Cube.")
-@click.argument('datasets',
-                type=click.Path(exists=True, readable=True, writable=True),
-                nargs=-1)
+
+@click.command(
+    help="Prepare Layered GeoTIFF ingestion into the Data Cube. Example command: python single_layer_tif_prepare.py /datacube/original_data/Colombia/ -l sr_refl -t LEDAPS -c LANDSAT_7 -i ETM -b 1=sr_band3 -b 2=sr_band4 -b 3=sr_band5 -b 4=sr_band7"
+)
+@click.argument('datasets', type=click.Path(exists=True, readable=True, writable=True), nargs=-1)
 @click.option('-l', '--processing_level', help='Set the processing level (ex. sr_refl).')
 @click.option('-t', '--product-type', help='Set the product type (ex. LEDAPS).')
 @click.option('-c', '--platform_code', help='Set the platform code (ex. LANDSAT_7).')
 @click.option('-i', '--instrument', help='Set the instrument.')
-@click.option('-b', '--bands', multiple=True,  help='Pass in a band number and the associated name (ex. -b 1=swir -b 2=red -b 3=blue ...)')
+@click.option(
+    '-b',
+    '--bands',
+    multiple=True,
+    help='Pass in a band number and the associated name (ex. -b 1=swir -b 2=red -b 3=blue ...)')
 def main(datasets, processing_level, product_type, platform_code, instrument, bands):
     arg_product_type = ''
     arg_platform_code = ''
-    arg_instrument = '' 
+    arg_instrument = ''
 
-    arg_processing_level = str(processing_level) if processing_level is not None else '0'
-    arg_product_type = str(product_type) if product_type is not None else None
-    arg_platform_code = str(platform_code) if platform_code is not None else None
-    arg_instrument = str(instrument) if instrument is not None else None
-    
+    arg_processing_level = str(processing_level) if processing_level is not None else 'sr_refl'
+    arg_product_type = str(product_type) if product_type is not None else 'LEDAPS'
+    arg_platform_code = str(platform_code) if platform_code is not None else 'LANDSAT_7'
+    arg_instrument = str(instrument) if instrument is not None else 'ETM'
+
+    arg_bands = bands if bands is not None else ["1=sr_band3", "2=sr_band4", "3=sr_band5", "4=sr_band7"]
+
     if arg_product_type is None or arg_platform_code is None:
-        print("Product Type and Platform Code must be included to run the script. Please include these parameters and run the script again.")
+        print(
+            "Product Type and Platform Code must be included to run the script. Please include these parameters and run the script again."
+        )
         sys.exit(2)
-    
+
     logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.DEBUG)
     print("Entering program")
     for dataset in datasets:
         path = Path(dataset)
         logging.info("Processing %s", path)
-        
+
         for file in os.listdir(str(path)):
             if file.endswith(".img") or file.endswith(".tif"):
                 print("Found file...")
@@ -195,7 +241,6 @@ def main(datasets, processing_level, product_type, platform_code, instrument, ba
                 with open(yaml_path, 'w') as stream:
                     yaml.dump(data, stream)
 
-            
+
 if __name__ == "__main__":
     main()
-            
